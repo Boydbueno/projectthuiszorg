92bd3113de11146ac67346083e0595a7d18e1ef7
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-10-10 19:52:44 +0200

    Added create users table migration

---
+++ app/database/migrations/2013_10_10_162522_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table)
+		{
+			$table->increments('id');
+			$table->string('email', 100);
+			$table->string('hash', 60);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}

1434b505dd4934bf1946ae1e8ec99de412a5815f
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-10-10 19:56:03 +0200

    Added users table seeder

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		// DB::table('users')->truncate();
+
+		$users = array(
+			array('email' => 'boydbuenodemesquita@gmail.com', 'hash' => Hash::make('Welkom01')),
+			array('email' => 'stefanweck1@gmail.com', 'hash' => Hash::make('Welkom01')),
+			array('email' => 'dmagphone@gmail.com', 'hash' => Hash::make('Welkom01'))
+		);
+
+		// Uncomment the below to run the seeder
+		DB::table('users')->insert($users);
+	}
+
+}

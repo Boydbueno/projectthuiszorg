e479ae30b72877a64ea2059ebb88eafb2e313991
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-10-14 16:51:43 +0200

    Added comments to routes.php

--- app/routes.php
+++ app/routes.php
@@ -6,10 +6,22 @@ Route::get('client', 'HomeController@showClient');
 
 Route::get('opdrachtgever', 'HomeController@showCompany');
 
+/*
+|---------------------------------------------------------------------------
+| Authorization
+|---------------------------------------------------------------------------
+*/
+
 Route::get('login', array('as' => 'login', 'uses' => 'AuthController@getLogin'));
 Route::post('login', 'AuthController@postLogin');
 Route::get('logout', array('as' => 'logout', 'uses' => 'AuthController@getLogout'));
 Route::controller('auth', 'AuthController');
+
+/*
+|---------------------------------------------------------------------------
+| Auth protected areas
+|---------------------------------------------------------------------------
+*/
 
 Route::group(array('before' => 'auth'), function() 
 {

b650aee319ad43cd41ae16ddbc134162434912dd
Stefan Weck <stefanweck1@gmail.com>
2013-10-16 10:51:25 +0200

    Added Login Form to Company View

--- app/views/company/index.blade.php
+++ app/views/company/index.blade.php
@@ -8,4 +8,13 @@
     <div class="welcome">
         <h1>Opdrachtgever</h1>
     </div>
+
+    @include('partials._login')
+
+    <div>
+    	<a href='/client/register'>Maak een account!</a>
+    </div>
+
+    @include('partials._errors')
+    
 @stop

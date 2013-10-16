4a6a07eaec16df206199ba042863f5a59b01157b
Stefan Weck <stefanweck1@gmail.com>
2013-10-16 11:46:03 +0200

    Added Contact page

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 	<div class="welcome">
 	    <h1>Dit is onze homepagina.</h1>
-	    <a href='/client'>Client</a> - <a href='/opdrachtgever'>Opdrachtgever</a>
+	    <a href='/client'>Client</a> - <a href='/opdrachtgever'>Opdrachtgever</a> - <a href='/contact'>Contact</a>
 	</div>
 @stop

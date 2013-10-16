c284c98d49d8fd3b79246667ebe0322692eee8db
Stefan Weck <stefanweck1@gmail.com>
2013-10-16 14:11:12 +0200

    Changed email and settings

--- app/views/emails/contact.blade.php
+++ app/views/emails/contact.blade.php
@@ -1,7 +1,13 @@
+<!DOCTYPE html>
 <html>
-	<head>
-	</head>
-	<body>
-		<p>Hooi</p>
-	</body>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Welcome to the website</h2>
+ 
+        <div>
+            Your sign up details are below:
+        </div>
+    </body>
 </html>

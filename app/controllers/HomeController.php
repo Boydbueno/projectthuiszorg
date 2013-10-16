e3e235a258a68d2b2b9c2662bbbd1280eda85f85
Stefan Weck <stefanweck1@gmail.com>
2013-10-16 11:46:44 +0200

    Changed routing, using HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,9 +2,19 @@
 
 class HomeController extends BaseController {
 
-	public function showIndex()
+	public function getIndex()
 	{
 		return View::make('home.index');
+	}
+
+	public function getContact()
+	{
+		return View::make('home.contact');
+	}
+
+	public function postContact()
+	{
+		return View::make('home.contact');
 	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,6 @@
 <?php
 
-Route::get('/', array('as' => 'home', 'uses' => 'HomeController@showIndex'));
-
+Route::controller('/', 'HomeController');
 Route::controller('client', 'ClientController');
 Route::controller('opdrachtgever', 'CompanyController');
 

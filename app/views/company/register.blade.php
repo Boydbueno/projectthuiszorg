f241f15c6bd4a5a3d7f43e7257ef82efcc6a41d9
Stefan Weck <stefanweck1@gmail.com>
2013-10-16 12:36:59 +0200

    Fixed errors not being in a section

--- app/views/company/register.blade.php
+++ app/views/company/register.blade.php
@@ -1,49 +1,51 @@
 @extends('layouts.master')
 
 @section('content')
-{{ Form::open(array('route' => 'clients.store')) }}
-	<ul>
-		<li>
-			{{ Form::label('first_name', 'Voornaam:') }}
-			{{ Form::text('first_name') }}
-		</li>
-		<li>
-			{{ Form::label('last_name', 'Achternaam:') }}
-			{{ Form::text('last_name') }}
-		</li>
-		<li>
-			{{ Form::label('email', 'Email:') }}
-			{{ Form::text('email') }}
-		</li>
-		<li>
-			{{ Form::label('street_name', 'Straatnaam:') }}
-			{{ Form::text('street_name') }}
-		</li>
-		<li>
-			{{ Form::label('house_number', 'Huisnummer:') }}
-			{{ Form::text('house_number') }}
-		</li>
-		<li>
-			{{ Form::label('zipcode', 'Postcode:') }}
-			{{ Form::text('zipcode') }}
-		</li>
-		<li>
-			{{ Form::label('city', 'Woonplaats:') }}
-			{{ Form::text('city') }}
-		</li>
-		<li>
-			{{ Form::label('password', 'Wachtwoord:') }}
-			{{ Form::password('password') }}
-		</li>
-		<li>
-			{{ Form::label('password_confirmation', 'Bevestig wachtwoord:') }}
-			{{ Form::password('password_confirmation') }}
-		</li>
-		<li>
-			{{ Form::submit() }}
-		</li>
-	</ul>
-{{ Form::close() }}
-@stop
 
-@include('partials._errors')
+	{{ Form::open(array('route' => 'clients.store')) }}
+		<ul>
+			<li>
+				{{ Form::label('first_name', 'Voornaam:') }}
+				{{ Form::text('first_name') }}
+			</li>
+			<li>
+				{{ Form::label('last_name', 'Achternaam:') }}
+				{{ Form::text('last_name') }}
+			</li>
+			<li>
+				{{ Form::label('email', 'Email:') }}
+				{{ Form::text('email') }}
+			</li>
+			<li>
+				{{ Form::label('street_name', 'Straatnaam:') }}
+				{{ Form::text('street_name') }}
+			</li>
+			<li>
+				{{ Form::label('house_number', 'Huisnummer:') }}
+				{{ Form::text('house_number') }}
+			</li>
+			<li>
+				{{ Form::label('zipcode', 'Postcode:') }}
+				{{ Form::text('zipcode') }}
+			</li>
+			<li>
+				{{ Form::label('city', 'Woonplaats:') }}
+				{{ Form::text('city') }}
+			</li>
+			<li>
+				{{ Form::label('password', 'Wachtwoord:') }}
+				{{ Form::password('password') }}
+			</li>
+			<li>
+				{{ Form::label('password_confirmation', 'Bevestig wachtwoord:') }}
+				{{ Form::password('password_confirmation') }}
+			</li>
+			<li>
+				{{ Form::submit() }}
+			</li>
+		</ul>
+	{{ Form::close() }}
+
+	@include('partials._errors')
+
+@stop

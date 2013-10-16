bc288a69cffa0b6dde29ec452445d96f93eee6bb
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-10-16 13:36:00 +0200

    Quick fix for registration

--- app/controllers/ClientController.php
+++ app/controllers/ClientController.php
@@ -40,4 +40,33 @@ class ClientController extends BaseController {
 
 	}
 
+		/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function postRegister()
+	{
+		$input = Input::all();
+
+		$validator = Validator::make($input, User::$rules);
+
+		if($validator->fails()) return Redirect::back()->withInput()->withErrors($validator);
+
+		$user = new User();
+
+		$user->email = $input['email'];
+		$user->password = Hash::make($input['password']);
+		$user->first_name = $input['first_name'];
+		$user->last_name = $input['last_name'];
+		$user->street_name = $input['street_name'];
+		$user->house_number = $input['house_number'];
+		$user->zipcode = $input['zipcode'];
+		$user->city = $input['city'];
+
+		$user->save();
+
+		return Redirect::to('/client');
+	}
+
 }

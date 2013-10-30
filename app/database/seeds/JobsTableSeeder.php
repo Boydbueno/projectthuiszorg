e5098c7a50cd72fc6f5006cdaa66b873d368cb8a
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-10-30 12:59:34 +0100

    Mofified jobs table seeder to represent migration changes

--- app/database/seeds/JobsTableSeeder.php
+++ app/database/seeds/JobsTableSeeder.php
@@ -11,21 +11,29 @@ class JobsTableSeeder extends Seeder {
 			array(
 				"title" => "Mutsen breien",
 				"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+				"amount" => 400,
+				"payment" => 1000.00,
 				"company_id" => 1
 			),
 			array(
 				"title" => "Tuin verbouwen",
 				"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+				"amount" => 400,
+				"payment" => 1000.00,
 				"company_id" => 1
 			),
 			array(
 				"title" => "T-shirts vouwen",
 				"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+				"amount" => 400,
+				"payment" => 1000.00,
 				"company_id" => 1
 			),
 			array(
 				"title" => "Vloer leggen",
 				"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+				"amount" => 400,
+				"payment" => 1000.00,
 				"company_id" => 1
 			)
 		);

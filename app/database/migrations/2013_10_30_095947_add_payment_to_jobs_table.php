87df141e113bdf00ae31ebb44fa04c9e95343945
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-10-30 11:32:33 +0100

    Added payment column to jobs table

---
+++ app/database/migrations/2013_10_30_095947_add_payment_to_jobs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddPaymentToJobsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('jobs', function(Blueprint $table) {
+			$table->double('payment', 10, 2);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('jobs', function(Blueprint $table) {
+			$table->dropColumn('payment');
+		});
+	}
+
+}

ed404f449160f9d0aec4662bf849258c19cb4a56
Stefan Weck <stefanweck1@gmail.com>
2013-11-01 15:27:55 +0100

    Converting Design to HTML/CSS

--- app/views/clients/index.blade.php
+++ app/views/clients/index.blade.php
@@ -5,24 +5,34 @@
 @stop
 
 @section('content')
-    <p>{{ Session::get('notice') }}</p> <!-- TODO: give a good spot -->
+    
+    <header class="block mainTitle floatFix">
+        <h1 class="floatLeft">Banending</h1>
+        <nav>
+            <ul class="mainMenu">
+                <li><a href="#" class="menuLink borderRight">Mijn Opdrachten</a></li>
+                <li><a href="#" class="menuLink borderRight">Instellingen</a></li>
+                <li><a href="#" class="menuLink">Uitloggen</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <section class="welcome">
+    <section class="block marginTop">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, ab iusto doloribus atque nostrum ullam velit porro distinctio architecto. 
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, ab iusto doloribus atque nostrum ullam velit porro distinctio architecto. <p>{{ Session::get('notice') }}</p>
         </p>
 
         <aside>
             <nav>
-            <ul>
-                <li><a href="#" class="btn">Mijn Opdrachten</a></li>
-                <li><a href="#" class="btn">Instellingen</a></li>
-            </ul>
+                <ul>
+                    <li><a href="#" class="btn">Mijn Opdrachten</a></li>
+                    <li><a href="#" class="btn">Instellingen</a></li>
+                </ul>
             </nav>
         </aside>
     </section> <!-- End Welcome -->
 
-    <section class="filters">
+    <section class="block marginTop">
         Filters here
     </section>
 
--- app/views/partials/jobs/_job.blade.php
+++ app/views/partials/jobs/_job.blade.php
@@ -1,15 +1,23 @@
-<article class="job">
-    <h2>{{ $job->title }}</h2>
+<article class="block marginTop">
+    <header class="mainTitle floatFix">
+        <h1 class="floatLeft">{{ $job->title }}</h1>
+        <span class="subTitle floatRight">Fysiek Werk</span>
+    </header>
     <div class="progress">
-        <div class="finished"></div>
+        <div class="progressBar"></div>
     </div>
-    <aside class="information">
-        <ul>
-            <li>Nog 5 dagen!</li>
-            <li>Gepauzeerd</li>
-            <li>€{{ $job->payment }}</li>
-        </ul>
+    <aside class="description">
+        <div class="information">
+            <p>The Earth was small, light blue, and so touchingly alone, our home that must be defended like a holy relic. The Earth was absolutely round. I believe I never knew what the word round meant until I saw Earth from space. Where ignorance lurks, so too do the frontiers of discovery and imagination. Here men from the planet Earth first set foot upon the Moon. July 1969 AD. We came in peace for all mankind.</p>
+        </div>
+        <div class="details">
+            <ul>
+                <li>Nog 5 dagen!</li>
+                <li>Gepauzeerd</li>
+                <li>€{{ $job->payment }}</li>
+            </ul>
 
-        {{ link_to_route('jobs.show', 'Bekijk Opdracht', array($job->id), array('class' => 'btn')) }}
+            {{ link_to_route('jobs.show', 'Bekijk Opdracht', array($job->id), array('class' => 'btn')) }}
+        </div>
     </aside>
 </article>

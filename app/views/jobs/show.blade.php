331ef82c373cc1342552df549ccc498e0cedf81e
Kevin Vlietstra <k-vlietstra@hotmail.com>
2013-11-01 12:24:51 +0100

    Corrected detail page

--- app/views/jobs/show.blade.php
+++ app/views/jobs/show.blade.php
@@ -1,32 +1,29 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Detail overzicht</title>
-</head>
-<body>
-	<div class="container">
+@extends('layouts.master')
+
+@section('title')
+	Vacatures - Detail overzicht
+@stop
+
+@section('content')
+	<div>
+		<a href="back">Terug</a>
+	</div>
+	<div>
+		<h1>asd</h1>
+	</div>
+	<div>
 		<div>
-			<a href="back">Terug</a>
+			<p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.</p> 
 		</div>
 		<div>
-			<h1>T-shirts opvouwen</h1>
-		</div>
-		<div>
-			<div>
-				<p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.</p> 
-			</div>
-			<div>
-				<ul>
-					<li>Nog 5 dagen</li>
-					<li>Gepauzeerd</li>
-					<li>€40,00</li>
-					<li>Albert Heijn</li>
-				</ul>
-				<p>Al 15 mensen gingen je voor, doe ook mee!</p>
-				<a href="#">Meedoen</a>
-			</div>
+			<ul>
+				<li>Nog 5 dagen</li>
+				<li>Gepauzeerd</li>
+				<li>€40,00</li>
+				<li>Albert Heijn</li>
+			</ul>
+			<p>Al 15 mensen gingen je voor, doe ook mee!</p>
+			<a href="#">Meedoen</a>
 		</div>
 	</div>
-</body>
-</html>
+@stop

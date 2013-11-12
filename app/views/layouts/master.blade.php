17140554e97eea34566cf244b70e7fcc23ab147f
Stefan Weck <stefanweck1@gmail.com>
2013-11-12 23:58:22 +0100

    Added small logo in heade

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,12 +12,12 @@
 	<div class="grid-container">
 
 		<header class="block mainMenu mainTitle floatFix">
-		    <h1 class="floatLeft">Banending</h1>
+		    <img class="floatLeft" src="images/logo_small.png" alt="Rework" /><h1 class="floatLeft">Rework</h1>
 		    <nav>
-		        <ul>
-		            <li><a href="#" class="menuLink borderRight">Mijn Opdrachten</a></li>
-		            <li><a href="#" class="menuLink borderRight">Instellingen</a></li>
-		            <li>{{ link_to_route("logout", "Uitloggen", array(), array("class" => "menuLink")) }}</li>
+		        <ul class="floatRight">
+		            <li class="floatLeft"><a href="#" class="menuLink borderRight">Mijn Opdrachten</a></li>
+		            <li class="floatLeft"><a href="#" class="menuLink borderRight">Instellingen</a></li>
+		            <li class="floatLeft">{{ link_to_route("logout", "Uitloggen", array(), array("class" => "menuLink")) }}</li>
 		        </ul>
 		    </nav>
 		</header>

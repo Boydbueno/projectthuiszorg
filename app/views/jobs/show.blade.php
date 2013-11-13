947511df41b67dd5e995e745f19b46598f83954b
Stefan Weck <stefanweck1@gmail.com>
2013-11-13 13:41:41 +0100

    Added styling for single Job page

--- app/views/jobs/show.blade.php
+++ app/views/jobs/show.blade.php
@@ -1,29 +1,39 @@
 @extends('layouts.master')
 
 @section('title')
-	Vacatures - Detail overzicht
+	Rework - {{ $job->title }}
 @stop
 
 @section('content')
-	<nav>
-		<a href="../clients">Terug</a>
+	<nav class='block marginTop mainTitle'>
+		<a href="../clients">Terug naar het overzicht</a>
 	</nav>
-	<section>
-		<h1>{{ $job->title }}</h1>
+
+	<section class='job'>
+		<article class="block marginTop floatFix {{ camel_case($job->jobcategory->label) }}">
+	    <header class="mainTitle floatFix">
+	        <h1 class="floatLeft">{{ $job->title }}</h1>
+	        <span class="subTitle floatRight">{{ $job->jobcategory->label }}</span>
+	    </header>
+	    <div class="progress">
+	        <div class="progressBar"></div>
+	    </div>
+	    <section class="description floatFix">
+	        <aside class="details floatRight">
+	            <ul>
+	                <li class="iconItem dateIcon bold">Nog 5 dagen!</li>
+	                <li class="iconItem timeIcon">Starten</li>
+	                <li class="iconItem moneyIcon">€ {{ $job->payment }},00</li>
+	                <li class="iconItem companyIcon">{{ $job->company->company_name }}</li>
+	            </ul>
+
+	            <p class="marginTop centerText">Al 15 mensen willen meewerken, doe ook mee!</p>
+				<a class="btn" href="#">Meedoen</a>
+	        </aside>
+	        <div class="information borderRight">
+	            <p>{{ $job->description }}</p>
+	        </div>
+	    </section>
 	</section>
-	<section>
-		<section>
-			<p>{{ $job->description }}}</p> 
-		</section>
-		<aside>
-			<ul>
-				<li>Nog 5 dagen</li>
-				<li>Gepauzeerd</li>
-				<li>€{{ $job->payment }}</li>
-				<li>{{ $job->company->company_name }}</li>
-			</ul>
-			<p>Al 15 mensen gingen je voor, doe ook mee!</p>
-			<a href="#">Meedoen</a>
-		</aside>
-	</section>
+</article>
 @stop

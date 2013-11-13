fd96f4d8525bffe24e3d210aae25d30b00d6093e
Stefan Weck <stefanweck1@gmail.com>
2013-11-13 14:10:47 +0100

    Added temp styling for homepage and related pages

--- app/views/client/index.blade.php
+++ app/views/client/index.blade.php
@@ -5,16 +5,20 @@
 @stop
 
 @section('content')
-    <div>
-        <h1>Client</h1>
-    </div>
 
-    @include('partials._login')
+	<section class="block description marginTop">
+	    <header>
+	        <h1>Client</h1>
+	    </header>
 
-    <div>
-    	<a href='/client/register'>Maak een account!</a>
-    </div>
+	    @include('partials._login')
 
-    @include('partials._errors')
+	    <div>
+	    	<a href='/client/register'>Maak een account!</a>
+	    </div>
+
+    	@include('partials._errors')
+
+    </section>
 
 @stop
--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.master')
 
 @section('content')
-	<div>
-	    <h1>Dit is onze homepagina.</h1>
-	    <a href='/client'>Client</a> - <a href='/opdrachtgever'>Opdrachtgever</a> - <a href='/contact'>Contact</a>
+	<div class="block marginTop">
+
+		<header class='mainTitle'>
+		    <h1>Welkom bij Rework!</h1>
+		</header>
+
+		<div class="progressSmall">
+        </div>
+
+		<section class="description floatFix">
+		
+		    <aside class="details floatRight">
+                <a class="btn" href='/client'>Client</a> <a class="btn" href='/opdrachtgever'>Opdrachtgever</a> <a class="btn" href='/contact'>Contact</a>
+            </aside>
+		    <p class="information borderRight">
+		        Wij zijn een nieuw initiatief genaamd Rework. Ons doel is om ouderen weer aan werk te helpen en het liefst in de branche waar ze vroeger werkzaam in waren. We willen dat de keuze en tijdsduur van een opdracht volledig bepaald kan worden door u! Geen verplichtingen en geen valkuilen. Heeft u nog tips of advies voor ons, wij horen het graag!
+		    </p>
+
+		</section>
 	</div>
 @stop

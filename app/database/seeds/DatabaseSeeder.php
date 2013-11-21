bf199d9a21e77aa97432d0f614fe4ffbb4cbb19e
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-11-21 01:59:01 +0100

    Added seeding for the job_user pivot table

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('JobcategoriesTableSeeder');
 		$this->call('JobUserTableSeeder');
 		$this->call('CompaniesTableSeeder');
+		$this->call('JobUserTableSeeder');
 	}
 
 }
--- app/database/seeds/JobUserTableSeeder.php
+++ app/database/seeds/JobUserTableSeeder.php
@@ -5,18 +5,12 @@ class JobUserTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('job_user')->truncate();
+		DB::table('job_user')->truncate();
 
 		$job_user = array(
 			array(
-				'job_id' => 1,
-				'user_id' => 1,
-				'amount' => 200
-			),
-			array(
-				'job_id' => 3,
-				'user_id' => 3,
-				'amount' => 300
+				'job_id' => Job::where('title', '=', 'Verven/tapijt leggen')->first()->id,
+				'user_id' => User::where('first_name', '=', 'Boyd')->first()->id
 			)
 		);
 

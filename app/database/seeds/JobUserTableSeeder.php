939270a94ba5883a8f53afe35cd4c14fa6546086
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-11-23 22:47:15 +0100

    Added some job_user seeds with amounts

--- app/database/seeds/JobUserTableSeeder.php
+++ app/database/seeds/JobUserTableSeeder.php
@@ -10,8 +10,34 @@ class JobUserTableSeeder extends Seeder {
 		$job_user = array(
 			array(
 				'job_id' => Job::where('title', '=', 'Verven/tapijt leggen')->first()->id,
-				'user_id' => User::where('first_name', '=', 'Boyd')->first()->id
-			)
+				'user_id' => User::where('first_name', '=', 'Boyd')->first()->id,
+				'amount' => 3
+			),
+			array(
+				'job_id' => Job::where('title', '=', 'Mutsen breien')->first()->id,
+				'user_id' => User::where('first_name', '=', 'Stefan')->first()->id,
+				'amount' => 300
+			),			
+			array(
+				'job_id' => Job::where('title', '=', 'Mutsen breien')->first()->id,
+				'user_id' => User::where('first_name', '=', 'Kevin')->first()->id,
+				'amount' => 200
+			),			
+			array(
+				'job_id' => Job::where('title', '=', 'Speelgoed monteren')->first()->id,
+				'user_id' => User::where('first_name', '=', 'Boyd')->first()->id,
+				'amount' => 20
+			),
+			array(
+				'job_id' => Job::where('title', '=', 'Speelgoed monteren')->first()->id,
+				'user_id' => User::where('first_name', '=', 'Stefan')->first()->id,
+				'amount' => 150
+			),			
+			array(
+				'job_id' => Job::where('title', '=', 'Speelgoed monteren')->first()->id,
+				'user_id' => User::where('first_name', '=', 'Kevin')->first()->id,
+				'amount' => 5
+			),
 		);
 
 		// Uncomment the below to run the seeder

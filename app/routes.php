3f7382134ec9f2ceb4ebab5052385704d852ab79
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-11-28 14:35:08 +0100

    Removed some unused routes and comments in routes file

--- app/routes.php
+++ app/routes.php
@@ -9,8 +9,6 @@ Route::controller('opdrachtgever', 'CompanyController');
 |---------------------------------------------------------------------------
 */
 
-// Route::get('login', array('as' => 'login', 'uses' => 'AuthController@getLogin'));
-// Route::post('login', 'AuthController@postLogin');
 Route::get('logout', array('as' => 'logout', 'uses' => 'AuthController@getLogout'));
 Route::controller('auth', 'AuthController');
 
@@ -23,14 +21,9 @@ Route::controller('auth', 'AuthController');
 Route::group(array('before' => 'auth'), function() 
 {
 
-	Route::get('protected', function(){
-		return View::make('protected');
-	});
-
 	Route::resource('clients', 'ClientsController');
-
 	Route::resource('jobs', 'JobsController');
-
+	
 });
 
 Route::controller('/', 'HomeController');

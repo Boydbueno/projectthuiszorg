3f92b49e80e15f1ed3b436a77143f7697de727ef
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-12-01 15:01:42 +0100

    Removed some unused actions in jobs controller

--- app/controllers/api/JobsController.php
+++ app/controllers/api/JobsController.php
@@ -13,26 +13,6 @@ class JobsController extends \BaseController {
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
 	 * Display the specified resource.
 	 *
 	 * @param  int  $id
@@ -41,39 +21,6 @@ class JobsController extends \BaseController {
 	public function show($id)
 	{
 		return \Job::find($id);
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
 	}
 
 }

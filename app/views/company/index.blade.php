cb35341d524519e5ec2aa2087fcabd6d30e003ac
Stefan Weck <stefanweck1@gmail.com>
2013-12-02 16:24:45 +0100

    Changed the design of these pages according to PSD

--- app/views/company/index.blade.php
+++ app/views/company/index.blade.php
@@ -1,25 +1,54 @@
-@extends('layouts.master')
+@extends('layouts.public')
 
 @section('title')
 	Rework - Opdrachtgever
 @stop
 
 @section('content')
-	
-	<section class="block description marginTop">
 
-	    <header>
-	        <h1>Opdrachtgever</h1>
-	    </header>
+<div class="backdropImage smallHeader">
+	{{ HTML::image("images/logo_big.png", "Rework Logo", array('class' => 'bigLogo')) }}
+</div>
 
-	    @include('partials._login')
+<section class="content grid-container">
 
-	    <div>
-	    	<a href='/opdrachtgever/register'>Maak een account!</a>
-	    </div>
+	<div class="posRelative">
+		<div class="imageOverlay floatFix">
+			<div class="block grid-60">
+				<h1 class="boxTitle">Welkom Opdrachtgever!</h1>
+				<p class="regularPadding">Wij zijn een nieuw initiatief genaamd Rework. Ons doel is om ouderen weer aan werk te helpen en het liefst in de branche waar ze vroeger werkzaam in waren. We willen dat de keuze en tijdsduur van een opdracht volledig bepaald kan worden door u! Geen verplichtingen en geen valkuilen.</p>
+			</div>
+			<div class="block grid-40 contactForm">
+				<header class="boxTitle">
+				    <h1>Inloggen</h1>
+				</header>
+				{{ Form::open(array('class' => 'regularPadding')) }}
+					<ul>
+						<li class="form-group">
+							{{ Form::label('email', 'Email:') }}
+							{{ Form::text('email') }}
+						</li>
 
-	    @include('partials._errors')
+						<li class="form-group">
+							{{ Form::label('password', 'Password:') }}
+							{{ Form::password('password') }}
+						</li>
 
-    </section>
+						<li class="form-group">
+							{{ Form::submit('Login', array('class' => 'btn btn-primary')) }}
+							{{ Session::get('notice') }}
+						</li>
+						<li>
+							<a href='/opdrachtgever/register'>Maak een account!</a>
+						</li>
+					</ul>
+				{{ Form::close() }}
+
+				@include('partials._errors')
+			</div>
+		</div>
+	</div>
+
+</section> <!-- End Content -->
 
 @stop
--- app/views/home/contact.blade.php
+++ app/views/home/contact.blade.php
@@ -1,35 +1,52 @@
-@extends('layouts.master')
+@extends('layouts.public')
 
 @section('content')
 
-	<div>
-	    <h1>Contact</h1>
+<div class="backdropImage smallHeader">
+	{{ HTML::image("images/logo_big.png", "Rework Logo", array('class' => 'bigLogo')) }}
+</div>
+
+<section class="content grid-container">
+
+	<div class="posRelative">
+		<div class="imageOverlay floatFix">
+			<div class="block grid-60">
+				<h1 class="boxTitle">Details</h1>
+				<p class="regularPadding">Wij zijn een nieuw initiatief genaamd Rework. Ons doel is om ouderen weer aan werk te helpen en het liefst in de branche waar ze vroeger werkzaam in waren. We willen dat de keuze en tijdsduur van een opdracht volledig bepaald kan worden door u! Geen verplichtingen en geen valkuilen.</p>
+			</div>
+			<div class="block grid-40 contactForm">
+				<header class="boxTitle">
+				    <h1>Contact</h1>
+				</header>
+				{{ Form::open(array('url' => '/contact', 'class' => 'regularPadding')) }} 
+					<ul>
+						<li>
+							{{ Form::label('first_name', 'Voornaam:') }}
+							{{ Form::text('first_name') }}
+						</li>
+						<li>
+							{{ Form::label('last_name', 'Achternaam:') }}
+							{{ Form::text('last_name') }}
+						</li>
+						<li>
+							{{ Form::label('email', 'Email:') }}
+							{{ Form::text('email') }}
+						</li>
+						<li>
+							{{ Form::label('text', 'Bericht:') }}
+							{{ Form::textarea('text') }}
+						</li>
+						<li>
+							{{ Form::submit('Verzend', array('class' => 'btn btn-primary center')) }}
+						</li>
+					</ul>
+				{{ Form::close() }}
+
+				@include('partials._errors')
+			</div>
+		</div>
 	</div>
 
-	{{ Form::open(array('url' => '/contact')) }}
-		<ul>
-			<li>
-				{{ Form::label('first_name', 'Voornaam:') }}
-				{{ Form::text('first_name') }}
-			</li>
-			<li>
-				{{ Form::label('last_name', 'Achternaam:') }}
-				{{ Form::text('last_name') }}
-			</li>
-			<li>
-				{{ Form::label('email', 'Email:') }}
-				{{ Form::text('email') }}
-			</li>
-			<li>
-				{{ Form::label('text', 'Bericht:') }}
-				{{ Form::textarea('text') }}
-			</li>
-			<li>
-				{{ Form::submit('Verzend', array('class' => 'btn btn-primary')) }}
-			</li>
-		</ul>
-	{{ Form::close() }}
-
-	@include('partials._errors')
+</section> <!-- End Content -->
 
 @stop

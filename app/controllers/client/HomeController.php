df04c547ab99ab8ac906e316192f9c6897e2cfde
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-12-03 13:36:25 +0100

    Moved registration actions to auth controller

--- app/controllers/client/HomeController.php
+++ app/controllers/client/HomeController.php
@@ -35,39 +35,5 @@ class HomeController extends \BaseController {
 
 		return \View::make('client.myjobs')->with('jobs', $jobs);
 	}
-
-	public function getRegister()
-	{
-		return \View::make('client.register');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function postRegister()
-	{
-		$input = \Input::all();
-
-		$validator = \Validator::make($input, \User::$rules);
-
-		if($validator->fails()) return \Redirect::back()->withInput()->withErrors($validator);
-
-		$user = new \User();
-
-		$user->email = $input['email'];
-		$user->password = \Hash::make($input['password']);
-		$user->first_name = $input['first_name'];
-		$user->last_name = $input['last_name'];
-		$user->street_name = $input['street_name'];
-		$user->house_number = $input['house_number'];
-		$user->zipcode = $input['zipcode'];
-		$user->city = $input['city'];
-
-		$user->save();
-
-		return \Redirect::to('/client');
-	}
-
+	
 }

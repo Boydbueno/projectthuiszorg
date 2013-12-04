ec52f95d9e81f98d3d18b5ff92d6439415641726
Stefan Weck <stefanweck1@gmail.com>
2013-12-04 22:47:26 +0100

    Added last name to the welcome message on the index page

--- app/views/client/index.blade.php
+++ app/views/client/index.blade.php
@@ -9,7 +9,7 @@
     <section class="block marginTop floatFix">
     
         <header class="mainTitle">
-            <h1>Welkom terug, {{ Auth::user()->first_name }}!</h1>
+            <h1>Welkom terug, {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}!</h1>
         </header>
 
         <div class="progressSmall">

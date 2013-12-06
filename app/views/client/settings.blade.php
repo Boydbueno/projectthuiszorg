d089c8b8c49ed140434b72dbf80b6e1c5169c50b
Stefan Weck <stefanweck1@gmail.com>
2013-12-06 10:37:41 +0100

    Changed some things to the settings page

--- app/views/client/settings.blade.php
+++ app/views/client/settings.blade.php
@@ -15,14 +15,14 @@
 		<div class="block marginTop floatFix">
 
 			<div class="grid-60 description">
-				<h1 class="boxTitle">Hallo!</h1>
-				<p class="regularPadding">Heeft u een vraag, probleem of suggestie over Rework. Wij staan altijd open voor feedback en helpen u graag met problemen. Ook staat er altijd een werknemer voor u klaar mocht u problemen hebben met het aanmaken van een account of het inloggen op de website! We zullen zo snel mogelijk reageren na het ontvangen van uw email!</p>
+				<h1>Hallo!</h1>
+				<p class="regularPadding marginTop">Heeft u een vraag, probleem of suggestie over Rework. Wij staan altijd open voor feedback en helpen u graag met problemen. Ook staat er altijd een werknemer voor u klaar mocht u problemen hebben met het aanmaken van een account of het inloggen op de website! We zullen zo snel mogelijk reageren na het ontvangen van uw email!</p>
 			</div>
 			<div class="grid-40 description">
-				<header class="boxTitle">
-				    <h1>Contact</h1>
+				<header>
+				    <h1>Gegevens Bewerken</h1>
 				</header>
-				{{ Form::open(array('url' => '/contact', 'class' => 'regularPadding')) }} 
+				{{ Form::open(array('url' => '/contact', 'class' => 'regularPadding marginTop contactForm')) }} 
 					<ul>
 						<li>
 							{{ Form::label('first_name', 'Voornaam:') }}

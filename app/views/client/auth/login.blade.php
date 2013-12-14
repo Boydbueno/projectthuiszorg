5a797d5e905284361e2b2cbec0e62abbc360b063
Stefan Weck <stefanweck1@gmail.com>
2013-12-14 19:49:01 +0100

    Added "create account" links to the login form

--- app/views/client/auth/login.blade.php
+++ app/views/client/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.public')
 
 @section('title')
-	Rework - Client
+	Rework - Meedoen
 @stop
 
 @section('content')
@@ -15,14 +15,17 @@
 	<div class="posRelative">
 		<div class="imageOverlay floatFix">
 			<div class="block grid-60">
-				<h1 class="boxTitle">Welkom Client!</h1>
-				<p class="regularPadding">Wij zijn een nieuw initiatief genaamd Rework. Ons doel is om ouderen weer aan werk te helpen en het liefst in de branche waar ze vroeger werkzaam in waren. We willen dat de keuze en tijdsduur van een opdracht volledig bepaald kan worden door u! Geen verplichtingen en geen valkuilen.</p>
+				<h1 class="boxTitle">Leuk dat u wilt meedoen!</h1>
+				<p class="regularPadding">Wij zijn een nieuw initiatief genaamd Rework. Ons doel is om ouderen weer aan werk te helpen en het liefst in de branche waar ze vroeger werkzaam in waren. We willen dat de keuze en tijdsduur van een opdracht volledig bepaald kan worden door u! Geen verplichtingen en geen valkuilen. Maak <a href='/client/register'>een account</a> of log in met uw bestaande account!</p>
 			</div>
 			<div class="block grid-40 contactForm">
 				<header class="boxTitle">
 				    <h1>Inloggen</h1>
 				</header>
-				{{ Confide::makeLoginForm()->render() }}
+				<div class="regularPadding">
+					{{ Confide::makeLoginForm()->render() }}
+					<a href='/client/register'>Maak een account!</a>
+				</div>
 			</div>
 		</div>
 	</div>

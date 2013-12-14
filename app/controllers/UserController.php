ce54837cd89e5b8ac6b32e276e31ae88aa4cce53
Stefan Weck <stefanweck1@gmail.com>
2013-12-14 20:17:12 +0100

    Added and styled the forgot password page

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -151,7 +151,7 @@ class UserController extends BaseController {
      */
     public function forgot_password()
     {
-        return View::make(Config::get('confide::forgot_password_form'));
+        return View::make('home.auth.password');
     }
 
     /**
---
+++ app/views/home/auth/password.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.public')
+
+@section('title')
+	Rework - Wachtwoord vergeten
+@stop
+
+@section('content')
+
+<div class="backdropImage smallHeader">
+	{{ HTML::image("images/logo_big.png", "Rework Logo", array('class' => 'bigLogo')) }}
+</div>
+
+<section class="content grid-container">
+
+	<div class="posRelative">
+		<div class="imageOverlay floatFix">
+			<div class="block grid-60">
+				<h1 class="boxTitle">Wachtwoord vergeten?</h1>
+				<p class="regularPadding">We weten hoe vervelend het kan zijn, daarom kunt u hiernaast uw email adres vullen, waarna u een mailtje zult krijgen om een nieuw wachtwoord aan te maken! U kunt dus weer bijna direct aan de slag, tot snel!</p>
+			</div>
+			<div class="block grid-40 contactForm">
+				<header class="boxTitle">
+				    <h1>Wachtwoord</h1>
+				</header>
+				<div class="regularPadding buttonTopMargin">
+					{{ Confide::makeForgotPasswordForm()->render() }}
+					<a href='/client/register'>Maak een account!</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section> <!-- End Content -->
+
+@stop

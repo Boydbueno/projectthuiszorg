f6d510d7251ce26a2f9d48b93896afbc90745497
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-12-19 00:15:47 +0100

    Added create statuses table migration

---
+++ app/database/migrations/2013_12_18_231330_create_statuses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('statuses', function($table)
+		{
+			$table->increments('id');
+			$table->string('label');
+			$table->string('description');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('statuses');
+	}
+
+}

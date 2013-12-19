3a5e729b5dbc24413909e400bd676d819dd665d4
Kevin Vlietstra <k-vlietstra@hotmail.com>
2013-12-19 16:26:30 +0100

    Slider now works

--- app/models/JobUser.php
+++ app/models/JobUser.php
@@ -3,8 +3,9 @@
 class JobUser extends Eloquent {
 
 	protected $table = 'job_user';
-	
 	protected $guarded = array();
+
+	public $timestamps = false;
 	public static $rules = array();
 
 	public function company()

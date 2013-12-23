d5e23deb35de590fc2805f141c2529ca8502f43b
Stefan Weck <stefanweck1@gmail.com>
2013-12-23 15:53:00 +0100

    Changed the client registration form

--- app/views/client/auth/register.blade.php
+++ app/views/client/auth/register.blade.php
@@ -22,29 +22,44 @@
 				<header class="boxTitle">
 				    <h1>Registreren</h1>
 				</header>
-				{{ Form::open(array('url' => 'client/register', 'class' => 'regularPadding')) }}
-					<ul>
-						<li>
-							{{ Form::label('email', 'Email:') }}
-							{{ Form::text('email') }}
-						</li>
-						<li>
-						</li>
-						<li>
-							{{ Form::label('password', 'Wachtwoord:') }}
-							{{ Form::password('password') }}
-						</li>
-						<li>
-							{{ Form::label('password_confirmation', 'Bevestig wachtwoord:') }}
-							{{ Form::password('password_confirmation') }}
-						</li>
-						<li>
-							{{ Form::submit('Registreer', array('class' => 'btn btn-primary')) }}
-						</li>
-					</ul>
-				{{ Form::close() }}
-				
-				@include('partials._errors')
+				<div class="regularPadding">
+					{{ Form::open() }}
+					
+					    <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
+					    <fieldset>
+					        <div class="form-group">
+					            <label for="email">{{{ Lang::get('confide::confide.e_mail') }}} <small>{{ Lang::get('confide::confide.signup.confirmation_required') }}</small></label>
+					            <input class="form-control" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+					        </div>
+					        <div class="form-group">
+					            <label for="password">{{{ Lang::get('confide::confide.password') }}}</label>
+					            <input class="form-control" type="password" name="password" id="password">
+					        </div>
+					        <div class="form-group">
+					            <label for="password_confirmation">{{{ Lang::get('confide::confide.password_confirmation') }}}</label>
+					            <input class="form-control" type="password" name="password_confirmation" id="password_confirmation">
+					        </div>
+
+					        @if ( Session::get('error') )
+					            <div class="alert alert-error alert-danger">
+					                @if ( is_array(Session::get('error')) )
+					                    {{ head(Session::get('error')) }}
+					                @endif
+					            </div>
+					        @endif
+
+					        @if ( Session::get('notice') )
+					            <div class="alert">{{ Session::get('notice') }}</div>
+					        @endif
+
+					        <div class="form-actions form-group">
+					          <button type="submit" class="btn btn-primary">{{{ Lang::get('confide::confide.signup.submit') }}}</button>
+					        </div>
+
+					    </fieldset>
+
+					{{ Form::close() }}
+				</div>
 			</div>
 		</div>
 	</div>

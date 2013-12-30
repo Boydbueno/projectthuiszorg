196e3d9759f22081f706f290184cba8db18e805b
Stefan Weck <stefanweck1@gmail.com>
2013-12-30 13:16:53 +0100

    Created a migration for the Pivot table Company User

---
+++ app/database/migrations/2013_12_30_112819_pivot_company_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class PivotCompanyUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('company_user', function(Blueprint $table) {
+			$table->increments('id');
+			$table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}

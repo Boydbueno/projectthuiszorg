f792674ac4b63ac64010d6c7a282d11ad7f2acb1
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-01-05 22:07:39 +0100

    Created and used footer partial

--- app/views/client/index.blade.php
+++ app/views/client/index.blade.php
@@ -76,38 +76,7 @@
     <!-- Todo: See if we can combine this with the php partial -->
     @include('partials.handlebars._job')
 
-    <section class="block marginTop">
-
-        <header class="mainTitle">
-            <h1>Footer</h1>
-        </header>
-
-        <div class="progressSmall">
-        </div>
-
-        <section class="description floatFix">
-            <aside class="details floatRight">
-                <nav>
-                    <ul>
-                        <li><a href="#" class="subMenuLink">Technisch werk</a></li>
-                        <li><a href="#" class="subMenuLink">Fysiek werk</a></li>
-                        <li><a href="#" class="subMenuLink">Adviserend werk</a></li>
-                        <li><a href="#" class="subMenuLink">Handenarbeid</a></li>
-                    </ul>
-                </nav>
-                <nav>
-                    <ul>
-                        <li><a href="#" class="subMenuLink">Terug naar boven</a></li>
-                    </ul>
-                </nav>
-            </aside>
-            
-            <p class="information borderRight">
-                Wij zijn een nieuw initiatief genaamd Rework. Ons doel is om ouderen weer aan werk te helpen en het liefst in de branche waar ze vroeger werkzaam in waren. We willen dat de keuze en tijdsduur van een opdracht volledig bepaald kan worden door u! Geen verplichtingen en geen valkuilen. Heeft u nog tips of advies voor ons, wij horen het graag!
-            </p>
-        </section>
-
-    </section>
+    @include('partials.client.footer')
 @stop
 
 @section('scripts')
--- app/views/client/myjobs.blade.php
+++ app/views/client/myjobs.blade.php
@@ -53,36 +53,6 @@
 
     </section> <!-- End Jobs -->
 
-    <section class="block marginTop">
+    @include('partials.client.footer')
 
-        <header class="mainTitle">
-            <h1>Footer</h1>
-        </header>
-
-        <div class="progressSmall">
-        </div>
-
-        <section class="description floatFix">
-            <aside class="details floatRight">
-                <nav>
-                    <ul>
-                        <li><a href="#" class="subMenuLink">Technisch werk</a></li>
-                        <li><a href="#" class="subMenuLink">Fysiek werk</a></li>
-                        <li><a href="#" class="subMenuLink">Adviserend werk</a></li>
-                        <li><a href="#" class="subMenuLink">Handenarbeid</a></li>
-                    </ul>
-                </nav>
-                <nav>
-                    <ul>
-                        <li><a href="#" class="subMenuLink">Terug naar boven</a></li>
-                    </ul>
-                </nav>
-            </aside>
-            
-            <p class="information borderRight">
-                Wij zijn een nieuw initiatief genaamd Rework. Ons doel is om ouderen weer aan werk te helpen en het liefst in de branche waar ze vroeger werkzaam in waren. We willen dat de keuze en tijdsduur van een opdracht volledig bepaald kan worden door u! Geen verplichtingen en geen valkuilen. Heeft u nog tips of advies voor ons, wij horen het graag!
-            </p>
-        </section>
-
-    </section>
 @stop

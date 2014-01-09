a03d5f18aebd04a6cddf61aff7ad9807e1f165c2
Stefan Weck <stefanweck1@gmail.com>
2014-01-09 23:28:00 +0100

    Small CSS fix to change appearance of button on company page

--- app/views/company/index.blade.php
+++ app/views/company/index.blade.php
@@ -19,7 +19,7 @@
             <aside class="floatRight quickMenu">
                 <nav>
                     <!-- TODO: Place 'mijn opdrachten' link in ul -->
-                    {{ link_to('client/myjobs', 'Mijn Opdrachten', array('class' => 'btn btnWorkIcon'))}}
+                    {{ link_to('client/myjobs', 'Mijn Opdrachten', array('class' => 'btn messageBoxBtn btnWorkIcon'))}}
                     <ul>
                         <li class="iconItem settingsIcon"><a href="#">Instellingen</a></li>
                     </ul>

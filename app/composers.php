10d9b04a0d759ba96f32cd3b1c5f220e41bdcb2f
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-01-24 14:35:54 +0100

    Moved filter view composer logic to own class

--- app/composers.php
+++ app/composers.php
@@ -1,17 +1,3 @@
 <?php
 
-View::composer('client.index', function($view)
-{
-	$dropdownPlaceholder = array('' => 'Categorie');
-	$jobcategories = $dropdownPlaceholder + Jobcategory::lists('label', 'id');
-
-	$jobavailability = [
-		'' => 'Beschikbaarheid',
-		'20' => 'Meer dan 20%',
-		'50' => 'Meer dan 50%',
-		'70' => 'Meer dan 70%',
-		'100' => 'Volledig'
-	];
-
-	$view->with('jobcategories', $jobcategories)->with('jobavailability', $jobavailability);
-});
+View::composer('client.index', 'Rework\Composers\FilterComposer');

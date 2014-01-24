52475703f29aec52b068e069a43753a7ed8a8cb2
Stefan Weck <stefanweck1@gmail.com>
2014-01-24 22:39:28 +0100

    Removed wrongly placed view composers

--- app/controllers/viewComposer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-dd("hoi");
-
-//Send friendlist data to each of these views
-View::composer(array('client.index','dashboard'), function($view)
-{
-    $view->with('friends', \Auth::user()->friendList());
-});

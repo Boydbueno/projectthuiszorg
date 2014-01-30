0af58ea74137fb6f11d00e2e974552195bba2b53
Stefan Weck <stefanweck1@gmail.com>
2014-01-30 18:58:52 +0100

    Added a rel with the job ID to detail page, for javascript use

--- app/views/client/jobDetail.blade.php
+++ app/views/client/jobDetail.blade.php
@@ -12,32 +12,32 @@
 
 	<section class='job'>
 		<article class="block marginTop floatFix {{ camel_case($job->jobcategory->label) }}">
-	    <header class="mainTitle floatFix">
-	        <h1 class="floatLeft">{{ $job->title }}</h1>
-	        <span class="subTitle floatRight">{{ $job->jobcategory->label }}</span>
-	    </header>
-	    <div class="progress">
-	        <div class="progressBar" style="width: {{ $job->percentageComplete }}%"></div>
-	        <div class="progressDescription">
-	            Nog {{ $job->amount_left }} {{ $job->postfix }}
-	        </div>
-	    </div>
-	    <section class="description floatFix">
-	        <aside class="details floatRight">
-	            <ul>
-	                <li class="iconItem dateIcon bold">
-	                	@if($job->daysLeft === 0)
-	                	    Alleen vandaag nog!
-	                	@else
-	                	    Nog {{ $job->daysLeft }} {{ $job->daysLeft  === 1 ? "dag" : "dagen" }}!
-	                	@endif
-	                </li>
-	                <li class="iconItem timeIcon">Starten</li>
-	                <li class="iconItem moneyIcon">€ {{ $job->payment }}</li>
-	                <li class="iconItem companyIcon">{{ $job->company->name }}</li>
-	            </ul>
+		<header class="mainTitle floatFix">
+			<h1 class="floatLeft">{{ $job->title }}</h1>
+			<span class="subTitle floatRight">{{ $job->jobcategory->label }}</span>
+		</header>
+		<div class="progress">
+			<div class="progressBar" style="width: {{ $job->percentageComplete }}%"></div>
+	 		<div class="progressDescription">
+				Nog {{ $job->amount_left }} {{ $job->postfix }}
+			</div>
+		</div>
+		<section class="description floatFix">
+			<aside class="details floatRight">
+				<ul>
+					<li class="iconItem dateIcon bold">
+						@if($job->daysLeft === 0)
+							Alleen vandaag nog!
+						@else
+							Nog {{ $job->daysLeft }} {{ $job->daysLeft  === 1 ? "dag" : "dagen" }}!
+						@endif
+					</li>
+					<li class="iconItem timeIcon">Starten</li>
+					<li class="iconItem moneyIcon">€ {{ $job->payment }}</li>
+					<li class="iconItem companyIcon">{{ $job->company->name }}</li>
+				</ul>
 
-	            <p class="marginTop centerText">{{ $job->participantsText }}</p>
+				<p class="marginTop centerText">{{ $job->participantsText }}</p>
 				@if(Session::get('notice'))
 					{{{ Session::get('notice') }}}
 
@@ -47,16 +47,16 @@
 					@endif
 				@endif
 
-	        </aside>
-	        <div class="information borderRight">
-	            <p>
-	            	{{ $job->long_description ? $job->long_description : $job->short_description }}
-	            </p>
-	        </div>
-	    </section>
+			</aside>
+			<div class="information borderRight">
+				<p>
+					{{ $job->long_description ? $job->long_description : $job->short_description }}
+				</p>
+			</div>
+		</section>
 	</section>
 
-	<section id="comments">
+	<section id="comments" rel="{{ $job->id }}">
 
 	</section>
 

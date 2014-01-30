3b72a00d4b7f2035949d9262fe46c7cc9a1be606
Stefan Weck <stefanweck1@gmail.com>
2014-01-30 18:57:42 +0100

    Added job and userinfo to a comment

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -3,4 +3,14 @@
 class Comment extends Eloquent {
 	protected $guarded = array();
 	public static $rules = array();
+
+	public function job()
+	{
+		return $this->belongsTo('Job');
+	}
+
+	public function user()
+	{
+		return $this->belongsTo('User')->with('userInfo');
+	}
 }

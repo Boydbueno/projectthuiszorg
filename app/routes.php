d57f80f49c10a008ef2c404e60cc23ee3df12104
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-01-30 20:30:14 +0100

    Cleaned up the routes file a bit by using shorthand array syntax. (PHP 5.4+)

--- app/routes.php
+++ app/routes.php
@@ -12,13 +12,13 @@ Route::post('/forgot_password', 'HomeController@do_forgot_password');
 |---------------------------------------------------------------------------
 */
 
-Route::group(array('prefix' => 'login'), function()
+Route::group(['prefix' => 'login'], function()
 {
 
-	Route::get('client', array('as' => 'client.login', 'uses' => 'controllers\client\AuthController@getLogin'));
-	Route::post('client', array('as' => 'client.doLogin', 'uses' => 'controllers\client\AuthController@postLogin'));
-	Route::get('company', array('as' => 'company.login', 'uses' => 'controllers\company\AuthController@getLogin'));
-	Route::post('company', array('as' => 'company.doLogin', 'uses' => 'controllers\company\AuthController@postLogin'));
+	Route::get('client', ['as' => 'client.login', 'uses' => 'controllers\client\AuthController@getLogin']);
+	Route::post('client', ['as' => 'client.doLogin', 'uses' => 'controllers\client\AuthController@postLogin']);
+	Route::get('company', ['as' => 'company.login', 'uses' => 'controllers\company\AuthController@getLogin']);
+	Route::post('company', ['as' => 'company.doLogin', 'uses' => 'controllers\company\AuthController@postLogin']);
 
 });
 
@@ -28,7 +28,7 @@ Route::group(array('prefix' => 'login'), function()
 |---------------------------------------------------------------------------
 */
 
-Route::group(array('prefix' => 'register'), function()
+Route::group(['prefix' => 'register'], function()
 {
 
 	Route::get('client', 'controllers\client\AuthController@getRegister');
@@ -43,22 +43,22 @@ Route::group(array('prefix' => 'register'), function()
 |---------------------------------------------------------------------------
 */
 
-Route::group(array('prefix' => 'client'), function()
+Route::group(['prefix' => 'client'], function()
 {
 
-	Route::group(array('before' => 'clientOrAdmin'), function() 
+	Route::group(['before' => 'clientOrAdmin'], function() 
 	{
-		Route::get('/', array('as' => 'client', 'uses' => 'controllers\client\HomeController@index'));
-		Route::get('jobs', array('as' => 'client.jobs', 'uses' => 'controllers\client\HomeController@getMyJobs'));
-		Route::get('settings', array('as' => 'client.settings', 'uses' => 'controllers\client\SettingsController@getSettings'));
-		Route::get('friendlist', array('as' => 'client.friendlist', 'uses' => 'controllers\client\FriendListController@getFriendList'));
+		Route::get('/', ['as' => 'client', 'uses' => 'controllers\client\HomeController@index']);
+		Route::get('jobs', ['as' => 'client.jobs', 'uses' => 'controllers\client\HomeController@getMyJobs']);
+		Route::get('settings', ['as' => 'client.settings', 'uses' => 'controllers\client\SettingsController@getSettings']);
+		Route::get('friendlist', ['as' => 'client.friendlist', 'uses' => 'controllers\client\FriendListController@getFriendList']);
 
-		Route::get('jobs/{id}', array('as' => 'client.jobs.show', 'uses' => 'controllers\client\JobsController@show'));
-		Route::get('jobs/{id}/join', array('as' => 'client.jobs.join', 'uses' => 'controllers\client\JobsController@join'));
+		Route::get('jobs/{id}', ['as' => 'client.jobs.show', 'uses' => 'controllers\client\JobsController@show']);
+		Route::get('jobs/{id}/join', ['as' => 'client.jobs.join', 'uses' => 'controllers\client\JobsController@join']);
 		Route::post('jobs/{id}/join', 'controllers\client\JobsController@postJoin');
 
-		Route::get('jobs/{id}/edit', array('as' => 'client.jobs.edit', 'uses' => 'controllers\client\JobsController@edit'));
-		Route::delete('jobs/{id}', array('as' => 'client.jobs.delete', 'uses' => 'controllers\client\JobsController@destroy')); // Doesn't destroy the job, just connection with user
+		Route::get('jobs/{id}/edit', ['as' => 'client.jobs.edit', 'uses' => 'controllers\client\JobsController@edit']);
+		Route::delete('jobs/{id}', ['as' => 'client.jobs.delete', 'uses' => 'controllers\client\JobsController@destroy']); // Doesn't destroy the job, just connection with user
 	});
 
 });
@@ -69,14 +69,14 @@ Route::group(array('prefix' => 'client'), function()
 |---------------------------------------------------------------------------
 */
 
-Route::group(array('prefix' => 'company'), function()
+Route::group(['prefix' => 'company'], function()
 {
 
-	Route::group(array('before' => 'companyOrAdmin'), function() 
+	Route::group(['before' => 'companyOrAdmin'], function() 
 	{
-		Route::get('/', array('as' => 'company', 'uses' => 'controllers\company\HomeController@index'));
+		Route::get('/', ['as' => 'company', 'uses' => 'controllers\company\HomeController@index']);
 
-		Route::get('jobs/create', array('as' => 'company.jobs.create', 'uses' => 'controllers\company\JobsController@create'));
+		Route::get('jobs/create', ['as' => 'company.jobs.create', 'uses' => 'controllers\company\JobsController@create']);
 		Route::post('jobs/create', 'controllers\company\JobsController@postCreate');
 	});
 
@@ -88,7 +88,7 @@ Route::group(array('prefix' => 'company'), function()
 |---------------------------------------------------------------------------
 */
 
-Route::group(array('prefix' => 'api'), function()
+Route::group(['prefix' => 'api'], function()
 {
 
 	# Comment routes
@@ -117,4 +117,4 @@ Route::group(array('prefix' => 'api'), function()
 Route::get( 'user/confirm/{code}',         'UserController@confirm');
 Route::get( 'user/reset_password/{token}', 'UserController@reset_password');
 Route::post('user/reset_password',         'UserController@do_reset_password');
-Route::get('logout', array('as' => 'logout', 'uses' => 'UserController@logout'));
+Route::get('logout', ['as' => 'logout', 'uses' => 'UserController@logout']);

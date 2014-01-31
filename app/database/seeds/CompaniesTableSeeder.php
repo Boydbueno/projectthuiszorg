8d2c6533a1de5b072cf2c7ad1141e916c4ecd100
Kevin Vlietstra <k-vlietstra@hotmail.com>
2014-01-31 13:53:53 +0100

    Added new companies and jobs.

--- app/database/seeds/CompaniesTableSeeder.php
+++ app/database/seeds/CompaniesTableSeeder.php
@@ -51,6 +51,39 @@ class CompaniesTableSeeder extends Seeder {
 				"city" => "Rotterdam",
 				"phone_number" => "010-1424237"
 			),
+			array(
+				"name" => "Primark",
+				"url" => "www.primark.nl/",
+				"description" => "Verkoop van damesmode, herenmode, kindermode en producten voor het interieur.",
+				"kvk_number" => 98741958,
+				"address" => "Nijmegen",
+				"house_number" => 143,
+				"zip_code" => "6511 NI",
+				"city" => "Nijmegen",
+				"phone_number" => "080-5247837"
+			),
+			array(
+				"name" => "Vestia",
+				"url" => "www.Vestia.nl",
+				"description" => "Wij bieden goede, betaalbare en duurzame huisvesting voor bijzondere doelgroepen en werken aan leefbaarheid en veiligheid.",
+				"kvk_number" => 57419829,
+				"address" => "Bijdorplaan",
+				"house_number" => 12,
+				"zip_code" => "2713 RR",
+				"city" => "Zoetermeer",
+				"phone_number" => "088-1242424"
+			),
+			array(
+				"name" => "Kledingreparatie Rotterdam",
+				"url" => "www.kledingreparatierotterdam.nl",
+				"description" => "Wij verzamelen oude tweedehands kleding en repareren het en sturen het dan op naar derdewereldlanden voor een goed doel.",
+				"kvk_number" => 57826429,
+				"address" => "Blaak",
+				"house_number" => 45,
+				"zip_code" => "3013 RP",
+				"city" => "Rotterdam",
+				"phone_number" => "010-5478424"
+			)
 		);
 
 		DB::table('companies')->insert($companies);
